289504a33acb9067e2cc0f5d827c8f3124edd63c
Athman Bakari <disguisedsandwich@gmail.com>
2021-04-14 14:07:35 +0300

    Add new permission and auth on api

--- courses/api/urls.py
+++ courses/api/urls.py
@@ -1,8 +1,17 @@
-from django.urls import path 
+from django.urls import path , include
+from rest_framework import routers
 from . import views
+
+router = routers.DefaultRouter()
+router.register('courses', views.CourseViewSet)
+
 app_name = 'courses'
 urlpatterns = [
         path('subjects/', views.SubjectListView.as_view(), name='subject_list'),
-        path('subjects/<pk>/', views.SubjectDetailView.as_view(), name='subject_detail')
+        path('subjects/<pk>/', views.SubjectDetailView.as_view(),
+            name='subject_detail'),
+        path('courses/<pk>/enroll/', views.CourseEnrollView.as_view(),
+            name='course_enroll'),
+        path('', include(router.urls))
         ]
 
--- courses/api/views.py
+++ courses/api/views.py
@@ -1,6 +1,11 @@
-from rest_framework import generics
-from ..models import Subject
-from .serializers import SubjectSerializer
+from rest_framework import generics,viewsets
+from ..models import Subject, Course
+from .serializers import SubjectSerializer, CourseSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.all()
@@ -10,3 +15,14 @@ class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
 
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+class CourseEnrollView(APIView):
+    authentication_classes = (BasicAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled':True})
