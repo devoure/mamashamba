46fbeea11aedc3ea97d10ec04a314b9dfc444fcd
Athman Bakari <disguisedsandwich@gmail.com>
2021-04-23 13:06:43 +0300

    Added context to chat server messages

--- mamashamba/settings.py
+++ mamashamba/settings.py
@@ -92,6 +92,15 @@ DATABASES = {
 }
 #asgi settings
 ASGI_APPLICATION = 'mamashamba.routing.application'
+
+CHANNEL_LAYERS = {
+        'default': {
+            'BACKEND':'channels_redis.core.RedisChannelLayer',
+            'CONFIG':{
+                'hosts':[('127.0.0.1', 6379)],
+                },
+            },
+        }
 #cache framework
 CACHES = {
         'default':{
